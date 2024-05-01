d04973a32ca360a833480a6c830f2677f31338d8
Stephanie Hays <steph@stephhays.dev>
2024-04-30 19:56:37 -0500

    Get Astro looking good

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: 'https://docs.chocolatey.org',
+    server: {
+        port: 5086,
+        open: "/en-us",
+        //host: true
+    },
+    redirects: {
+        '/': '/en-us',
+    }
+});
